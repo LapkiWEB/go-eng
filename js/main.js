07d1695ab1aaac292de474aced4e3c8db6b1cc5e
Stepan <Stefan.irk96@mail.ru>
2021-11-03 13:37:16 +0800

    add menu header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-// $( function () {
+$(function () {
+
+    $('.menu__btn').on('click', (function () {
+        if (!$('.menu__wrapper').hasClass('openDone')) {
+            $('.menu__wrapper').addClass('openDone');
+        } else {
+            $('.menu__wrapper').removeClass('openDone');
+        }
+        if (!$('.menu__btn').hasClass('active')) {
+            $('.menu__btn').addClass('active');
+        } else {
+            $('.menu__btn').removeClass('active');
+        }
+    }));
 
 
-// })
+})
