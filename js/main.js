0ae7ccc51c90858d55e2a65d1f7bcdecb3a341ab
Stepan <Stefan.irk96@mail.ru>
2021-11-04 12:50:57 +0800

    finish1 no mail.php

--- js/main.js
+++ js/main.js
@@ -15,3 +15,39 @@ $(function () {
 
 
 })
+
+$(document).ready(function () {
+    $(".link-popup").magnificPopup();
+
+    $(document).ready(function () {
+        $("#form").submit(function () {
+            $.ajax({
+                type: "POST",
+                url: "mail.php",
+                data: $(this).serialize()
+            }).done(function () {
+                $(this).find("input").val("");
+                alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+                $("#form").trigger("reset");
+            });
+            return false;
+        });
+    });
+
+
+    $(document).ready(function () {
+        $("#forms").submit(function () {
+            $.ajax({
+                type: "POST",
+                url: "mail.php",
+                data: $(this).serialize()
+            }).done(function () {
+                $(this).find("input").val("");
+                alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+                $("#forms").trigger("reset");
+            });
+            return false;
+        });
+    });
+    
+});
